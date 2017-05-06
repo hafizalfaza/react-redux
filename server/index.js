a69a8bafa878ef945f2d61a8bc4e76c9739856c2
hafizalfaza <hafizalfaza@gmail.com>
2017-05-06 12:31:50 +0700

    React-webpack init.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 import express from 'express';
 import path from 'path';
 
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackConfig from '../webpack.config.dev';
+
 const app = express();
+
+const compiler = webpack(webpackConfig);
+
+app.use(webpackMiddleware(compiler));
 
 app.get('/*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'index.html'));
