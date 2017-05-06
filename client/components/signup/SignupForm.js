783fcc88c4201fdf65a60a0bc04be8209010dd5e
hafizalfaza <hafizalfaza@gmail.com>
2017-05-06 18:14:25 +0700

    Client side validations.

--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -2,6 +2,8 @@ import React from 'react';
 import map from 'lodash/map';
 import timezones from '../../data/timezones';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import validateInput from '../../../server/shared/validations/signup';
 
 class SignupForm extends React.Component{
 	constructor(props){
@@ -11,7 +13,8 @@ class SignupForm extends React.Component{
 			email: '',
 			password: '',
 			passwordConfirmation: '',
-			timezone: ''
+			timezone: '',
+			errors: {}
 		}
 		
 		this.onChange = this.onChange.bind(this);
@@ -23,19 +26,36 @@ class SignupForm extends React.Component{
 		this.setState({[e.target.name]: e.target.value});
 	}
 	
+	isValid(){
+		const {errors, isValid} = validateInput(this.state);
+		
+		if(!isValid){
+			this.setState({errors});
+		}
+		
+		return isValid
+	}
+	
 	onSubmit(e){
 		e.preventDefault();
-		this.props.userSignupRequest(this.state);
+		this.setState({errors: {}});
+		if(this.isValid()){
+			this.props.userSignupRequest(this.state).then(
+				() => {},
+				(err) => this.setState({errors: err.response.data})
+			);
+		}
 	}
 	
 	render(){
+		const {errors} = this.state;
 		const options = map(timezones, (key, val) =>
 			<option key={val} value={val}>{key}</option>
 		);
 		return (
 			<form onSubmit={this.onSubmit}>
 				<h1>Register Now!</h1>
-				<div className="form-group">
+				<div className={classnames("form-group", {'has-error': errors.username})}>
 					<label className="control-label">
 						Username
 					</label>
@@ -46,9 +66,10 @@ class SignupForm extends React.Component{
 						value={this.state.username}
 						onChange={this.onChange}
 					/>
+					{errors.username && <span className="help-block">{errors.username}</span>}
 				</div>
 				
-				<div className="form-group">
+				<div className={classnames("form-group", {'has-error': errors.email})}>
 					<label className="control-label">
 						Email
 					</label>
@@ -59,9 +80,10 @@ class SignupForm extends React.Component{
 						value={this.state.email}
 						onChange={this.onChange}
 					/>
+					{errors.email && <span className="help-block">{errors.email}</span>}
 				</div>
 				
-				<div className="form-group">
+				<div className={classnames("form-group", {'has-error': errors.password})}>
 					<label className="control-label">
 						Password
 					</label>
@@ -72,9 +94,10 @@ class SignupForm extends React.Component{
 						value={this.state.password}
 						onChange={this.onChange}
 					/>
+					{errors.password && <span className="help-block">{errors.password}</span>}
 				</div>
 				
-				<div className="form-group">
+				<div className={classnames("form-group", {'has-error': errors.passwordConfirmation})}>
 					<label className="control-label">
 						Confirm Password
 					</label>
@@ -85,9 +108,10 @@ class SignupForm extends React.Component{
 						value={this.state.passwordConfirmation}
 						onChange={this.onChange}
 					/>
+					{errors.passwordConfirmation && <span className="help-block">{errors.passwordConfirmation}</span>}
 				</div>
 				
-				<div className="form-group">
+				<div className={classnames("form-group", {'has-error': errors.timezone})}>
 					<label className="control-label">
 						Timezone
 					</label>
@@ -100,6 +124,7 @@ class SignupForm extends React.Component{
 					<option value='' disabled>Choose Your Timezone</option>
 					{options}
 					</select>
+					{errors.timezone && <span className="help-block">{errors.timezone}</span>}
 				</div>
 				<div className="form-group">
 					<button className="btn btn-primary btn-lg">
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,50 +1,14 @@
 import express from 'express';
-import Validator from 'validator';
-import isEmpty from 'lodash/isEmpty';
+import validateInput from '../shared/validations/signup';
 
 const router = express.Router();
-
-function validateInput(data){
-	let errors = {};
-	
-	if(Validator.isEmpty(data.username)){
-		errors.username = "This field is required";
-	}	
-	
-	if(Validator.isEmpty(data.email)){
-		errors.email = "This field is required";
-	}else{
-		if(!Validator.isEmail(data.email)){
-			errors.email = "This email is invalid";
-		}
-	}
-	
-	if(Validator.isEmpty(data.password)){
-		errors.password = "This field is required";
-	}	
-	
-	if(Validator.isEmpty(data.passwordConfirmation)){
-		errors.passwordConfirmation = "This field is required";
-	}else{
-		if(!Validator.equals(data.password, data.passwordConfirmation)){
-			errors.passwordConfirmation = "Passwords must match";
-		}
-	}
-	
-	if(Validator.isEmpty(data.timezone)){
-		errors.timezone = "This field is required";
-	}	
-	
-	return {
-		errors,
-		isValid: isEmpty(errors)
-	}
-}
 
 router.post('/', (req, res) => {
 	const {errors, isValid} = validateInput(req.body);
 	
-	if(!isValid){
+	if(isValid){
+		res.json({success: true});
+	}else{
 		res.status(400).json(errors);
 	}
 });
